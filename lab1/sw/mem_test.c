bd189c084627567b32157cc84dc0d671613ec9ca
SleepBook <oar.yin@sjtu.edu.cn>
2018-02-05 01:43:53 +0000

    Fixed a Major BUG
    Previous virtual address calculation has a flaw inside, when
    calculating the byte address(before do a right shift to get the
    word address, offset should multiply by 4 first.
    
    The core is that, in C, the abstract addressing(offset), is word(type)
    addressed, by in lower level, it's always address(byte addressable), so
    when going back and forth between these two layers, always do the
    byte/word trasform

--- lab1/sw/mem_test.c
+++ lab1/sw/mem_test.c
@@ -1,3 +1,11 @@
+/*
+ * EE382N.4 Lab1 Mem Chcek
+ * WEnqi Yin
+ *
+ * haven't add zero address test
+ * led stuff omitted for now
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
@@ -8,48 +16,160 @@
 #define MAP_SIZE 4096UL
 #define MAP_MASK (MAP_SIZE - 1)
 
+//IO is lined to LED here, the low 8 bits are map to leds
+#define IO_ADDR 0x41200008
 #define PORT_A_ADDR 0x40000000
-#define PORT_B_ADDR 0x40002000
-#define ADDR_LSFR_DATA 0x43C00000
-#define ADDR_LSFR_CTRL 0x43C00004
-#define DATA_LSFR_DATA 0x43C00000
-#define DATA_LSFR_CTRL 0x43C00004
+#define PORT_B_ADDR 0x40001000
+#define ADDR_LSFR 0x43C00000
+#define DATA_LSFR 0x43C01000
+#define LSFR_CTRL_OFFSET 8
+
+#define INTERV_MASK 15
 #define TEST_LENGTH 1024
 
+#define DEBUG 
 typedef struct cell{
-    unsigned long port;
-    unsigned long data;
-    unsigned long inter;
+    unsigned data;
+    unsigned inter;
+#ifdef DEBUG
+    volatile unsigned int *v_addr;
+#endif
 } cell;
 
-void main()
+int main()
 {
-    //use 2 bits to represent r/w port
-    //0 for port A, 1 for port B
-    int port_flag;
+    /*
+     * Least Two bits of Flag to represent R/W port
+     * 0 for port A, 1 for port B
+     */
+    int port_flag = 0;
 
-    FILE* log;
-    FILE* diag;
+    FILE* log, *diag;
     log = fopen("log.txt","w");
     diag = fopen("report.txt", "w");
-    int count;
 
+    int count = 0;
+    int i;
     cell status[TEST_LENGTH];
+    for(i=0;i<TEST_LENGTH;i++){
+        status[i].data = 0;
+        status[i].inter = 0;
+    }
 
-    //Mem Maping
-    int fd = open("/dev/mem", 0_RDWR|0_SYNC, S_IRUSR);
+
+    /*
+     * Open Phy Mem
+     */
+    int fd = open("/dev/mem", O_RDWR|O_SYNC);
     if(fd == -1){
         printf("unable to open /dev/mem");
         return -1;
     }
 
+    /*
+     * DO memory Maping
+     */
+
+    volatile unsigned int *d_lsfr_base, *a_lsfr_base;
+    volatile unsigned int *d_lsfr_ctrl, *a_lsfr_ctrl;
+    volatile unsigned int *porta_base, *portb_base;
     
+    d_lsfr_base = (unsigned int *)mmap(NULL, MAP_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fd, DATA_LSFR & ~MAP_MASK);
+    a_lsfr_base = (unsigned int *)mmap(NULL, MAP_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fd, ADDR_LSFR & ~MAP_MASK);
+    d_lsfr_ctrl = d_lsfr_base + ((((DATA_LSFR + LSFR_CTRL_OFFSET))&MAP_MASK) >> 2);
+    a_lsfr_ctrl = a_lsfr_base + ((((ADDR_LSFR + LSFR_CTRL_OFFSET))&MAP_MASK) >> 2);
 
-    
+    porta_base = (unsigned int *)mmap(NULL, MAP_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fd, PORT_A_ADDR & ~MAP_MASK);
+    portb_base = (unsigned int *)mmap(NULL, MAP_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fd, PORT_B_ADDR & ~MAP_MASK); 
 
 
+    /* Main loop for Mem Test
+     */
+    int error_flag = 0;
+    while(1){
+        unsigned int data;
+        unsigned int mem_offset;
+        unsigned int interval;
+        unsigned int temp, i;
+        volatile unsigned int *addr;
+        fprintf(log, "================================\n");
+        fprintf(log, "Cycle Count %d\n", count);
 
+        if(count == TEST_LENGTH + INTERV_MASK - 1) break;
+        else if(count < TEST_LENGTH - 1){
+            //Refresh HW LSFR to get new data
+            *a_lsfr_ctrl = 0;
+            *a_lsfr_ctrl = 1;
+            *d_lsfr_ctrl = 0;
+            *d_lsfr_ctrl = 1;
 
+            data = *d_lsfr_base;
+#ifdef DEBUG
+            //data = 4189497481;
+            //data = count;
+#endif
+            mem_offset = *a_lsfr_base;
+            interval = data & INTERV_MASK;
+            if(!interval) interval = 7;
+           
 
-    
+            //create record
+            status[mem_offset].data = data;
+            status[mem_offset].inter = interval;
+
+            //writing phase
+            if((port_flag >> 1) & 0x1){
+                addr = portb_base + ((((PORT_B_ADDR) + mem_offset*4)&MAP_MASK) >> 2);
+            }
+            else{
+                addr = porta_base + ((((PORT_A_ADDR) + mem_offset*4)&MAP_MASK) >> 2);
+            }
+#ifdef DEBUG
+            status[mem_offset].v_addr = addr;
+#endif
+            *addr = data;
+            fprintf(log, "Writing %u to mem offset %u\n", data, mem_offset);
+        }
+
+        //readback & check phase
+        for(i=1;i<TEST_LENGTH;i++){
+            if(status[i].inter == 0) continue;
+            else if(status[i].inter == 1){
+                if(port_flag & 0x1){
+                    addr = portb_base + ((((PORT_B_ADDR) + i*4)&MAP_MASK) >> 2);
+                }
+                else{
+                    addr = porta_base + ((((PORT_A_ADDR) + i*4)&MAP_MASK) >> 2);
+                }
+                data = *addr;
+                fprintf(log, "Readback data from offset %u, the value is %u\n", i, data);
+                if(data != status[i].data){
+#ifdef DEBUG
+                    printf("at address %u:\n", i);
+                    printf("old value is %u, readin value is %u\n", status[i].data, data);
+                    printf("the write in addr is %p, the readout addr is %p\n", (void*)status[i].v_addr, (void*)addr);
+#endif
+                    error_flag = 1;
+                    /*
+                     * And you could do some fancy LED stuff
+                     */
+                    fprintf(diag, "Defect Detected at MEM_OFFSET %u\n",i);
+                    fprintf(log, "Defect Detected at MEM_OFFSET %u\n",i);
+                }
+            }
+            status[i].inter--;
+        }
+        count++;
+    }
+
+    if(!error_flag){
+        fprintf(diag, "Mem Test PASSED\n");
+    }
+
+    close(log);
+    close(diag);
+    close(fd);
+    munmap(NULL, MAP_SIZE);
+    return 0;
+}
 
