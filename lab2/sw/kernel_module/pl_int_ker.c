8b516502d4793411cccbfcf960ffcd82c575537a
baby-panda <qw2699@utexas.edu>
2018-03-19 14:54:44 +0000

    [device node] now could be created w/o mknod system call

--- lab2/sw/kernel_module/pl_int_ker.c
+++ lab2/sw/kernel_module/pl_int_ker.c
@@ -22,36 +22,44 @@
 #include <linux/mman.h>
 #include <linux/slab.h>
 #include <linux/ioport.h>
-#include <linux/types.h>  // mode_t, dev_t
-#include <linux/kdev_t.h>  // MKDEV
-#include <linux/fcntl.h>  // mknod
-#include <linux/stat.h>
-#include <linux/unistd.h>
-#include <asm/uaccess.h>  // for get_fs(), set_fs()
+
+#include <linux/types.h>
+#include <linux/kdev_t.h>
+#include <asm/uaccess.h>
+#include <linux/platform_device.h>
+#include <linux/device.h>
+#include <linux/cdev.h>
 
 #define MODULE_VER "1.0"
 #define INTERRUPT 164
-#define MODULE_NM "fpga_interrupt_peripheral"
+#define MODULE_NM "fpga_int"
 
 #undef DEBUG
-#define DEBUG
+//#define DEBUG
 
-static struct proc_dir_entry *interrupt_arm_file;
-static struct fasync_struct *fasync_fpga_queue ;
-static int major;
+static struct proc_dir_entry *proc_file;
+static struct fasync_struct *fasync_queue ;
+static struct cdev chr_dev;  /* Global variable for the character device structure */
+static struct class *p_devclass;  /* Global variable for the device class */
+static int errno;
+static dev_t dev_num;  /* Global variable for the first device number */
 
+
+/*
+ * This function is called when the associated interrupt raises
+ */
 irqreturn_t interrupt_handler(int irq, void *dev_id)
 {  
 #ifdef DEBUG
-    printk(KERN_INFO "fpga_int_peripheral: Interrupt detected in kernel \n");
+  printk(KERN_INFO "[KM %s] Interrupt detected in kernel \n", MODULE_NM);
 #endif
   
 /* 
  * Signal the user application that an interupt occured
  * NOTICE the name kill in Linux usually is assocaited with 
  * sending signals
- */  
-  kill_fasync(&fasync_fpga_queue, SIGIO, POLL_IN);
+ */
+  kill_fasync(&fasync_queue, SIGIO, POLL_IN);
   return 0;
 }
 
@@ -59,22 +67,22 @@ irqreturn_t interrupt_handler(int irq, void *dev_id)
 /*
  * This function is called when the fpga_int device is opened
  */
-static int fpga_open (struct inode *inode, struct file *file) {
+static int fpga_int_open (struct inode *inode, struct file *file) {
 #ifdef DEBUG
-    	printk(KERN_INFO "fpga_int_peripheral: Inside fpga_open \n");
+  printk(KERN_INFO "[KM %s] Inside %s_open \n", MODULE_NM, MODULE_NM);
 #endif
-	return 0;
+  return 0;
 }
 
 
 /*
  * This function is called when the fpga_int device is released
  */
-static int fpga_release (struct inode *inode, struct file *file) {
+static int fpga_int_release (struct inode *inode, struct file *file) {
 #ifdef DEBUG
-    	printk(KERN_INFO "\nfpga_int: Inside fpga_release \n");  // DEBUG
+  printk(KERN_INFO "[KM %s] Inside %s_release \n", MODULE_NM, MODULE_NM);
 #endif
-	return 0;
+  return 0;
 }
 
 
@@ -87,12 +95,12 @@ static int fpga_release (struct inode *inode, struct file *file) {
  * kill_fasync(), a SIGIO is dispatched to the owning application.
  * Actually i am not clear how this helper function ensures that
  */
-static int fpga_fasync (int fd, struct file *filp, int on)
+static int fpga_int_fasync (int fd, struct file *filp, int on)
 {
 #ifdef DEBUG
-    	printk(KERN_INFO "\nfpga_int: Inside fpga_fasync \n");  // DEBUG
+  printk(KERN_INFO "[KM %s] Inside %s_fasync \n", MODULE_NM, MODULE_NM);
 #endif
-	return fasync_helper(fd, filp, on, &fasync_fpga_queue);
+  return fasync_helper(fd, filp, on, &fasync_queue);
 } 
 
 
@@ -102,147 +110,171 @@ static int fpga_fasync (int fd, struct file *filp, int on)
 *  Define which file operations are supported
 *
 */
-struct file_operations fpga_fops = {
-	.owner	=	THIS_MODULE,
-	.llseek	=	NULL,
-	.read	=	NULL,
-	.write	=	NULL,
-	.poll	=	NULL,
-	.unlocked_ioctl	=	NULL,
-	.mmap	=	NULL,
-	.open	=	fpga_open,
-	.flush	=	NULL,
-	.release=	fpga_release,
-	.fsync	=	NULL,
-	.fasync	=	fpga_fasync,
-	.lock	=	NULL,
-	.read	=	NULL,
-	.write	=	NULL,
+struct file_operations fpga_int_fops = {
+  .owner = THIS_MODULE,
+  .llseek = NULL,
+  .read = NULL,
+  .write = NULL,
+  .poll = NULL,
+  .unlocked_ioctl = NULL,
+  .mmap = NULL,
+  .open = fpga_int_open,
+  .flush = NULL,
+  .release = fpga_int_release,
+  .fsync = NULL,
+  .fasync = fpga_int_fasync,
+  .lock = NULL,
+  .read = NULL,
+  .write = NULL,
 };
 
 static const struct file_operations proc_fops = {
-    .owner  = THIS_MODULE,
-    .open   = NULL,
-    .read   = NULL,
+  .owner = THIS_MODULE,
+  .open = NULL,
+  .read = NULL,
 };
      
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * function: init_interrupt_arm
+ * function: init_fpga_int
  *
  * This function creates the /proc directory entry interrupt_arm. It
  * also configures the parallel port then requests interrupt 240 from Linux.
  */
  
-static int __init init_interrupt_arm(void)
+static int __init init_fpga_int(void)
 {
 
- int rv = 0;
- mm_segment_t saved_fs;
- 
-  printk("FPGA Interrupt Module\n");
-  printk("FPGA Driver Loading.\n");
+#ifdef DEBUG
+  printk("[KM %s] Start initializing \n", MODULE_NM);
+#endif
 	
-  major = register_chrdev(0, MODULE_NM, &fpga_fops);
-  if (0 > major) {
-	printk("fpga_int: unable to get major. ABORTING!\n");
-	return -EBUSY;
-	}
-  printk("Using Major Number %d on %s\n", major, MODULE_NM);
-
-
-  //rv = mknod("/dev/fpga_interrupt_peripheral", S_IFCHR | S_IRWXU | S_IRWXG | S_IRWXO,  MKDEV(major, 0));
-  // http://opensourceforu.com/2011/04/character-device-files-creation-operations/
-  //printk("SYS_mknod=%d\n", __NR_mknod);
-  saved_fs = get_fs();
-  set_fs(get_ds());
-  asm(
-          "mov r0, %1;"
-          "mov r1, %2;"
-          "mov r2, %3;"
-          "lsl r2, #8;"
-          "mov r7, #14;" // /usr/include/arm-linux-gnueabihf/asm/unistd.h
-          "swi #0;"
-          //"add %0, %0, $2;"
-          "mov %0, r0;"
-          :
-          "=r" (rv)
-          :
-          "r" ("/dev/fpga_interrupt_peripheral"),
-          "r" (S_IFCHR | S_IRWXU | S_IRWXG | S_IRWXO),
-          "r" (major)
-          :
-          "r7"
-     );
-  set_fs(saved_fs);
-  if(!rv)
-    printk("mknod error: %d\n", rv);
-  interrupt_arm_file = proc_create("interrupt_arm", 0444, NULL, &proc_fops );
+  /* Step 0: create proc entry */
+  proc_file = proc_create("fpga_int", 0444, NULL, &proc_fops );
   
-  if(interrupt_arm_file == NULL) {
-  	printk("fpga_int: create /proc entry returned NULL. ABORTING!\n");
-    return -ENOMEM;
+  if(NULL == proc_file) {
+    printk(KERN_ALERT "[KM %s] create /proc entry returned NULL. ABORTING! \n", MODULE_NM);
+    goto proc_create_failed;
   }
 
- // request interrupt from linux
+  /* Step 1: allocate character device region */
+  errno = alloc_chrdev_region( &dev_num, 0, 1, MODULE_NM);
+
+  if(0 > errno)
+  {
+    printk(KERN_ALERT "[KM %s] Device Registration error: %d \n", MODULE_NM, errno);
+    return -EBUSY;
+  }
+#ifdef DEBUG
+  else
+    printk(KERN_ALERT "[KM %s] Major Number is %d \n", MODULE_NM, dev_num >> 20);
+#endif
+
+  /* Step 2: create character device class */
+  p_devclass = class_create(THIS_MODULE, "chardev");
+  if(NULL == p_devclass)
+  {
+    printk(KERN_ALERT "[KM %s] Class creation failed \n", MODULE_NM);
+    goto devclass_create_failed;
+  }
+
+  /* Step 3: create character device */
+  if(NULL == device_create(p_devclass, NULL, dev_num, NULL, MODULE_NM))
+  {
+    printk(KERN_ALERT "[KM %s] Device creation failed \n", MODULE_NM);
+    goto dev_create_failed;
+  }
+
+  /* Step 4: initialize character device and add it to device class */
+  cdev_init(&chr_dev, &fpga_int_fops);
+
+  if(-1 == cdev_add(&chr_dev, dev_num, 1))
+  {
+    printk(KERN_ALERT "[KM %s] Device addition failed \n", MODULE_NM);
+    goto dev_add_failed;
+  }
+
+ /* Step 5: bind interrupt from linux */
  
-  rv = request_irq(INTERRUPT, interrupt_handler, IRQF_TRIGGER_RISING,
-                   "interrupt_arm", NULL);
+  errno = request_irq(INTERRUPT, interrupt_handler, IRQF_TRIGGER_RISING,
+                   "fpga_int", NULL);
   
-  if ( rv ) {
-    printk("Can't get interrupt %d\n", INTERRUPT);
-    goto no_interrupt_arm;
+  if(errno)
+  {
+    printk(KERN_ALERT "[KM %s] Can't get interrupt %d \n", MODULE_NM, INTERRUPT);
+    goto irq_request_failed;
   }
-
 
 /* everything initialized */
-  printk(KERN_INFO "%s %s Initialized\n",MODULE_NM, MODULE_VER);
+#ifdef DEBUG
+  printk(KERN_INFO "[KM %s] %s Initialized\n ",MODULE_NM, MODULE_VER);
+#endif
   return 0;
 
+irq_request_failed:
+dev_add_failed:
+dev_create_failed:
+  class_destroy(p_devclass);
+devclass_create_failed:
+  unregister_chrdev_region(dev_num, 1);
 /* remove the proc entry on error */
-  no_interrupt_arm:
-  remove_proc_entry("interrupt_arm", NULL);
+  remove_proc_entry("fpga_int", NULL);
   return -EBUSY;
+
+proc_create_failed:
+  return -ENOMEM;
+
 }
 
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * function: cleanup_interrupt_arm
+ * function: cleanup_fpga_int
  *
  * This function frees interrupt 164 then removes the /proc directory entry 
  * interrupt_arm. 
  */
  
-static void __exit cleanup_interrupt_arm(void)
+static void __exit cleanup_fpga_int(void)
 {
-  mm_segment_t saved_fs;
-
-/* free the interrupt */
+  /* Step -5: free the interrupt */
   free_irq(INTERRUPT,NULL);
-  
-  unregister_chrdev(major, MODULE_NM);
+#ifdef DEBUG
+  printk(KERN_INFO "[KM %s] interrupt %d released \n", MODULE_NM, INTERRUPT);
+#endif
 
-  remove_proc_entry("interrupt_arm", NULL);
-  printk(KERN_INFO "%s %s removed\n", MODULE_NM, MODULE_VER);
-  //printk("SYS_unlink=%d\n", __NR_unlink);
-  saved_fs = get_fs();
-  set_fs(get_ds());
-  asm(
-          "mov r0, %0;"
-          "mov r7, #10;" // /usr/include/arm-linux-gnueabihf/asm/unistd.h
-          "swi #0;"
-          :
-          :
-          "r" ("/dev/fpga_interrupt_peripheral")
-          :
-          "r7"
-     );
-  set_fs(saved_fs);
-  printk(KERN_INFO "/dev/fpga_interrupt_peripheral removed\n");
+  /* Step -4: remove device from class */
+  device_destroy(p_devclass, dev_num);
+#ifdef DEBUG
+  printk(KERN_INFO "[KM %s] device removed \n", MODULE_NM);
+#endif
+
+  /* Step -3: device delete */
+  cdev_del(&chr_dev);
+#ifdef DEBUG
+  printk(KERN_INFO "[KM %s] device deleted \n", MODULE_NM);
+#endif
+
+  /* Step -2: destroy device class */
+  class_destroy(p_devclass);
+#ifdef DEBUG
+  printk(KERN_INFO "[KM %s] device class destroied \n", MODULE_NM);
+#endif
+
+  /* Step -1: unregister character device region */
+  unregister_chrdev_region(dev_num, 1);
+#ifdef DEBUG
+  printk(KERN_INFO "[KM %s] device region unregisted \n", MODULE_NM);
+#endif
+
+  /* Step -0: remove proc entry */
+  remove_proc_entry("fpga_int", NULL);
+#ifdef DEBUG
+  printk(KERN_INFO "[KM %s] /dev/fpga_int removed \n", MODULE_NM);
+#endif
 }
 
-module_init(init_interrupt_arm);
-module_exit(cleanup_interrupt_arm);
+module_init(init_fpga_int);
+module_exit(cleanup_fpga_int);
 
 MODULE_AUTHOR("Wenqi Yin");
 MODULE_DESCRIPTION("pl_interrupt handler module");
